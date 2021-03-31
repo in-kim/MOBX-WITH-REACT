466836b7c0e878805300de728f3206a6594441bf
in-kim <ehddls960617@naver.com>
2021-03-31 09:39:36 +0900

    store import 페이지

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'mobx-react'; // MobX 에서 사용하는 Provider
 import App from './App';
+import CounterStore from './store/countDecorator'; // 방금 만든 스토어 불러와줍니다.
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const counter = new CounterStore(); // 스토어 인스턴스를 만들고
+
+ReactDOM.render(
+  <Provider counter={counter}>
+    {/* Provider 에 props 로 넣어줍니다. */}
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
